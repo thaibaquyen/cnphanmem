e5f5b973eb53cd5ee1da24287cfb8f276174ff9d
thaibaquyen <48308419+thaibaquyen@users.noreply.github.com>
2019-03-12 19:55:30 +0700

    ok
    ok

---
+++ b2gtbt/scrip.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function hienthi(){
+	document.getElementById('gt1').style.display='inline-block';
+	document.getElementById('gt2').style.display='inline-block';
+	document.getElementById('gt4').style.display='inline-block';
+	document.getElementById('gt5').style.display='inline-block';
+}
